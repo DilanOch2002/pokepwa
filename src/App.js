b8c52917101dd91cc7a6330db4d774325e61c33d
DilanOch2002 <106693124+DilanOch2002@users.noreply.github.com>
2025-11-10 11:24:46 -0500

    notificaciones v8

--- src/App.js
+++ src/App.js
@@ -107,7 +107,8 @@ function App() {
     };
 
     fetchAllPokemon();
-  }, [allPokemon]);
+  }, [allPokemon]); // ✅ SOLO CORRE UNA VEZ
+
 
   // ✅ Buscar Pokémon (individual o listado)
 const handleSearch = async () => {
